68eee69b655941ab3088c961672643d6e161d3b8
Lenar9108 <lenar9108@mail.ru>
2026-01-04 00:53:26 +0300

    change event clean model from table to incremental strategy

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -4,3 +4,9 @@ user_id,
 "timestamp",
 type_id
 from {{ source("scooters_raw", "events") }}
+where
+{% if is_incremental() %}
+    "timestamp" > (select max("timestamp") from {{ this }})
+{% else %}
+    "timestamp" < date '2023-08-01'
+{% endif %}
